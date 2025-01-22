cf7eede7e81a6e1bdaedb076709a1f2854fd4248
LUCAS HENRIQUE PEREIRA SILVA <lucas.hpsilva6@senacsp.edu.br>
2025-01-22 17:19:41 -0300

    funções

---
+++ fundamentos/operadores.js
@@ -0,0 +1,39 @@
+/**
+ * Operadores
+ * Aritmedicos
+ * Aritmeticos de Atribuição
+ * Comparação
+ * Logicos
+ */
+
+console.clear()
+console.log("Operadores Aritmedicos")
+console.log("+ | soma")
+console.log("- | Subtração")
+console.log("* | Multiplição")
+console.log("/| Divisão")
+console.log("% | modulo (Resto de Divisão")
+console.log("")
+
+console.log("+=| soma e atribuição a variavel")
+console.log("-= | Subtração e atribuição a variavel")
+console.log("*=| Multiplição e atribuição a variavel")
+console.log("/= Divisão  e atribuição a variavel")
+console.log("++ | soma 1 ao resultado")
+console.log("-- | subtrair 1 do resultado")
+
+console.log("")
+console.log("Comparadores")
+console.log("> | maior que")
+console.log("< | menor que")
+console.log(">=| maior ou igual a")
+console.log("<=| maior ou igual a")
+console.log("=== |indêntico a")
+console.log("!==| não indêntico a ")
+
+console.log("")
+console.log("Operadore Logicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("!  | NOT")
+
